bf2d09e39cddbbfebceeede546eef349e8204ecc
Paul Lorett Amazona <whatevergeek@gmail.com>
2019-03-15 22:49:00 +0800

    initial files

---
+++ test_arithmetic_properties_with_randomized_inputs.py
@@ -0,0 +1,21 @@
+from arithmetic import *
+import pytest
+from hypothesis import given, settings
+import hypothesis.strategies as st
+
+@given(st.integers(), st.integers())
+@settings(max_examples=500)
+def test_commutative_property(factor1, factor2):
+    assert multiply(factor1,factor2) == multiply(factor2,factor1)    
+
+@given(st.integers(), st.integers(), st.integers())
+def test_associative_property(factor1, factor2, factor3):
+    assert multiply(multiply(factor1, factor2), factor3) == multiply(factor1, multiply(factor2,factor3))
+
+@given(st.integers())
+def test_identity_property(factor1):
+    assert multiply(factor1,1) == factor1 
+
+@given(st.integers(), st.integers(), st.integers())
+def test_distributive_property(num1, num2, num3):
+    assert multiply(num1,(num2 + num3)) == multiply(num1,num2) + multiply(num1,num3)
